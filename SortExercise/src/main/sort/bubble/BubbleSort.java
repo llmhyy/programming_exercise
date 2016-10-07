0cbf949920f87c07a55773ad0b0409a3dad5c724
jelsaoverlandfrost <JelsaOverlandFrost@gmail.com>
2016-10-07 14:26:35 +0800

    finish bubble sort

--- SortExercise/src/main/sort/bubble/BubbleSort.java
+++ SortExercise/src/main/sort/bubble/BubbleSort.java
@@ -4,8 +4,21 @@ public class BubbleSort {
 
 	public int[] sort(int[] array) {
 		// TODO I am going to do it
-		System.out.println("hello world");
-		return null;
+		int l = array.length - 1; // Initiate a length variable for later use
+		int t; // Initiate a temporary variable
+		for (int i = 0; i < l; i++) { // Count the times of overview
+			for (int k = 0; k < l-i; k++) { // Overview the array except for the last i items
+				if (array[k] > array[k+1]) { // Change if [k] is greater than [k+1]
+					t = array[k];
+					array[k] = array[k+1];
+					array[k+1] = t;
+				};
+			}
+		};	
+		/*for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		};*/
+		return array;
 	}
 	
 }
